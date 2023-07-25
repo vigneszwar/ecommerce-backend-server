2539d5595042c1096f103527b202f8859af25836
ns-2002 <look4nandinisingh@gmail.com>
2023-07-25 21:14:56 +0530

    Changes to be committed: 	modified:   sample_python_file/sample.py

--- sample_python_file/sample.py
+++ sample_python_file/sample.py
@@ -1,3 +1,7 @@
+HEAD
 print("this is my python file pushed to github and you are pulling it and running in your system. This line is modified")
 print("This is my change");
+
+
+print("this is my python file pushed to github and you are pulling it and running in your system.This is modified.")
 
